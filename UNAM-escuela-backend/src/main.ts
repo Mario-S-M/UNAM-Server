21d92df63bfa2f8a9ff82fc1c5fa76b064ab22c5
Mario-S-M <mayitolalito@hotmail.com>
2025-06-13 12:55:13 -0600

    Redireccionamiento al puerto añternativo

--- UNAM-escuela-backend/src/main.ts
+++ UNAM-escuela-backend/src/main.ts
@@ -3,17 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
     origin: [
       'http://localhost',
       'http://localhost:3000',
       'http://localhost:3001',
+      'http://localhost:11434',
       'http://132.247.186.91',
       'http://132.247.186.91:3000',
       'http://132.247.186.91:80',
+      'http://132.247.186.91:11434',
       'http://132.247.186.91',
-      "http://frontend",
+      'http://frontend',
       /^http:\/\/132\.247\.186\.91(:\d+)?$/,
     ],
     credentials: true,

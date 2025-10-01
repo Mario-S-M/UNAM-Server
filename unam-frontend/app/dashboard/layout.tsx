4c570a8fc8f3bf4411d18a2b1735849df72521c0
Mario-S-M <mayitolalito@hotmail.com>
2025-10-01 14:28:42 -0600

    remove dashboard from layout

--- unam-frontend/app/dashboard/layout.tsx
+++ unam-frontend/app/dashboard/layout.tsx
@@ -15,7 +15,6 @@ export default function SideBarLayout({ children }: { children: React.ReactNode 
         <main className="flex-1">
           <div className="flex items-center gap-2 p-4 border-b">
             <SidebarTrigger className="h-8 w-8" />
-            <h1 className="text-lg font-semibold">Dashboard</h1>
           </div>
           <div className="p-4">
             {children}
